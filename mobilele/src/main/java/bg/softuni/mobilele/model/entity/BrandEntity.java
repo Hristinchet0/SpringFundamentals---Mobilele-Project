3841e1abae6b93759dfaf5ed34de57f23b32cd92
Hristinchet0 <bansko.sport@gmail.com>
2023-08-02 16:04:55 +0300

    Add

--- mobilele/src/main/java/bg/softuni/mobilele/model/entity/BrandEntity.java
+++ mobilele/src/main/java/bg/softuni/mobilele/model/entity/BrandEntity.java
@@ -1,15 +1,19 @@
 package bg.softuni.mobilele.model.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Table(name = "brands")
 public class BrandEntity extends BaseEntity {
 
-    @Column(name = "name")
+    @Column(name = "name", nullable = false)
     private String name;
 
     @Column(name = "created")
@@ -17,6 +21,9 @@ public class BrandEntity extends BaseEntity {
 
     @Column(name = "modified")
     private LocalDate modified;
+
+    @OneToMany(mappedBy = "brand", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private List<ModelEntity> models;
 
     public BrandEntity() {
     }
@@ -43,5 +50,23 @@ public class BrandEntity extends BaseEntity {
 
     public void setModified(LocalDate modified) {
         this.modified = modified;
+    }
+
+    public List<ModelEntity> getModels() {
+        return models;
+    }
+
+    public void setModels(List<ModelEntity> models) {
+        this.models = models;
+    }
+
+    @Override
+    public String toString() {
+        return "BrandEntity{" +
+                "name='" + name + '\'' +
+                ", created=" + created +
+                ", modified=" + modified +
+                ", models=" + models +
+                '}';
     }
 }
--- mobilele/src/main/java/bg/softuni/mobilele/model/entity/ModelEntity.java
+++ mobilele/src/main/java/bg/softuni/mobilele/model/entity/ModelEntity.java
@@ -2,23 +2,29 @@ package bg.softuni.mobilele.model.entity;
 
 import bg.softuni.mobilele.model.entity.enums.CategoryEnum;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "models")
 public class ModelEntity extends BaseEntity{
 
-    @Column(name = "name")
+    @Column(name = "name", nullable = false)
     private String name;
 
+    @Column(name = "category", nullable = false)
     @Enumerated(EnumType.STRING)
     private CategoryEnum category;
 
-    @Column(name = "image_url")
+    @Column(name = "image_url", nullable = false)
     private String imageUrl;
 
-    @Column(name = "start_year")
+    @Column(name = "start_year", nullable = false)
     private Integer startYear;
 
     @Column(name = "end_year")
@@ -29,4 +35,77 @@ public class ModelEntity extends BaseEntity{
 
     @ManyToOne
     private BrandEntity brand;
+
+    public ModelEntity() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public CategoryEnum getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoryEnum category) {
+        this.category = category;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Integer getStartYear() {
+        return startYear;
+    }
+
+    public void setStartYear(Integer startYear) {
+        this.startYear = startYear;
+    }
+
+    public Integer getEndYear() {
+        return endYear;
+    }
+
+    public void setEndYear(Integer endYear) {
+        this.endYear = endYear;
+    }
+
+    public LocalDate getModified() {
+        return modified;
+    }
+
+    public void setModified(LocalDate modified) {
+        this.modified = modified;
+    }
+
+    public BrandEntity getBrand() {
+        return brand;
+    }
+
+    public void setBrand(BrandEntity brand) {
+        this.brand = brand;
+    }
+
+
+    @Override
+    public String toString() {
+        return "ModelEntity{" +
+                "name='" + name + '\'' +
+                ", category=" + category +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", startYear=" + startYear +
+                ", endYear=" + endYear +
+                ", modified=" + modified +
+                ", brand=" + (brand != null ? brand.getName() : null) +
+                '}';
+    }
 }
