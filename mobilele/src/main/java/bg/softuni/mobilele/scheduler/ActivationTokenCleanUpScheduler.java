f5c1e72e9714271f2f70bf80de3a0e9203e0bf49
Hristinchet0 <bansko.sport@gmail.com>
2023-08-24 00:25:54 +0300

    Enable scheduling...todo...

---
+++ mobilele/src/main/java/bg/softuni/mobilele/scheduler/ActivationTokenCleanUpScheduler.java
@@ -0,0 +1,15 @@
+﻿package bg.softuni.mobilele.scheduler;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ActivationTokenCleanUpScheduler {
+
+//    //todo
+//    @Scheduled(cron = "0 5 * * * ?")
+//    public void cleanUpTokens() {
+//        //todo - call the user service
+//    }
+
+}
